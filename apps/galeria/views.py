999649f82f479e31e8b9018242915ff6253aae70
Vinicius Gabriel <viniciusrtycbn@gmail.com>
2024-06-10 08:05:48 -0300

    0.1

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -29,7 +29,7 @@ def buscar(request):
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
-    return render(request, "galeria/buscar.html", {"cards": fotografias})
+    return render(request, "galeria/index.html", {"cards": fotografias})
 
 def nova_imagem(request):
     if not request.user.is_authenticated:
@@ -44,8 +44,30 @@ def nova_imagem(request):
             return redirect('index')
     return render(request, 'galeria/nova_imagem.html', {'form': form})
 
-def editar_imagem(request):
-    pass
+def editar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    form = FotografiaForms(instance=fotografia)
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia editada com sucesso!')
+            return redirect('index')
+    return render(request, 'galeria/editar_imagem.html', {'form':form, 'foto_id': foto_id})
 
 def deletar_imagem(request):
-    pass
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, 'Deleção feita com sucesso!')
+    return redirect('index')
+
+def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+    return render(request, "galeria/index.html", {"cards": fotografias})
